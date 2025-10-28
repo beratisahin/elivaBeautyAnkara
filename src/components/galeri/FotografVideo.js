d98bed3f82de10b53b008eabd0409b8c6af5cf0f
beratisahin <beratisahin94@gmail.com>
2025-10-28 20:35:01 +0300

    git commit -m "instagram"

--- src/components/galeri/FotografVideo.js
+++ src/components/galeri/FotografVideo.js
@@ -2,7 +2,7 @@ import "./FotografVideo.css";
 import React from 'react';
 import useDocumentTitle from "../../useDocumentTitle";
 import Title from "../title/Title";
-import Instagram from "./lavinyaProfil.webp"
+import Instagram from "./elivaIG.png"
 
 export default function FotografVideo() {
     useDocumentTitle('Fotoğraflar- Ankara Eliva Beauty');
@@ -14,7 +14,7 @@ export default function FotografVideo() {
                />
                 <br/>
                 <p className="instaLink" style={{textAlign:"center",color:"gray"}}>Detaylı güncel fotoğraf ve videolar için <a className="instagram"  href="https://www.instagram.com/elivabeautyankara/" target="_blank">Ankara Eliva Beauty</a> instagram profilimizi ziyaret edebilirsiniz. </p>
-                <img src={Instagram} style={{width:"95%",height:"auto"}}></img>
+                <img src={Instagram} style={{width:"95%",height:"auto"}}> </img>
             </div>
             
         </div>
